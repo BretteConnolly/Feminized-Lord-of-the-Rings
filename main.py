7493be2a0f82df15c87e2e40561c6fe0c0957938
Brette Fitzgibbon <79230918+BretteConnolly@users.noreply.github.com>
2022-07-30 01:10:46 +0000

    Added comments

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-list_of_words = [' he ', ' she ', 'He ', 'She ','man ', 'woman ', ' son ', ' daughter ', 'father', 'mother', 'brother', 'sister', 'boy', 'girl', ' lad ', ' lass ', 'sir', 'madam', 'Mr.', 'Miss', ' him ', ' her ', ' his ', ' her ', 'himself', 'herself', 'His', 'Her', 'fellow', 'sister', 'sistership', 'sisterhood', 'men ', 'women ', 'Men', 'Women', 'lady', 'lord', ' king', ' queen', 'prince ', 'princess ', 'Daddy', 'Mommy', 'Dad', 'Mom', 'beard ', 'hair ', 'uncle', 'aunt', 'Uncle', 'Aunt', 'nephew', 'niece', ' him,', ' her,', ' him?', ' her?', ' him:', ' her:', ' him;', ' her;', ' him.', ' her.', 'Him', 'Her', '(him ', '(her ', ' him)', ' her)', ' he,', ' she,', ' he?', ' she?', ' he:', ' she:', ' he;', 'she;', ' he.', ' she.', '(he ', '(she ', ' he)', ' she)', 'man,', 'woman,', 'man?', 'woman?', 'man;', 'woman;', 'man:', 'woman:', 'man.', 'woman.', 'man)', 'woman)', ' King', ' Queen', 'Lady', 'Lord', 'emperor', 'empress', 'Mrs.', 'Miss', ' son,', ' daughter,', ' sons', ' daughters', 'princes ', 'princesses ', ' lads ', ' lasses ', 'beards', 'hair']
-
-book = open("text_data.txt")
+from list_of_words import word_list
+book = open("text_data.txt") #original transcript
 book_contents = book.read()
 feminized_book = open("Feminized LOTR.txt", "a")
 index = 0
-while index < len(list_of_words):
-  if index == 0:
-    new_book = book_contents.replace(list_of_words[index], list_of_words[index + 1])
-    index += 2
+while index < len(word_list):
+  if index == 0: #base case
+    new_book = book_contents.replace(word_list[index], word_list[index + 1])
+    index += 2 #words occur in pairs: a masculine and a feminine
   else:
-    new_book = new_book.replace(list_of_words[index], list_of_words[index + 1])
+    new_book = new_book.replace(word_list[index], word_list[index + 1]) #recursion to replace multiple words from the source material, one pair at a time
     index += 2
 feminized_book.write(new_book)
 
